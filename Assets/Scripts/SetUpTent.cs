2e5a65eb6bcc74a0a07b6970c4fa9fc53f225be4
Tyler Molander <tylermolander@gmail.com>
2018-09-27 23:44:44 -0400

    CanvasUI

--- Assets/Scripts/SetUpTent.cs
+++ Assets/Scripts/SetUpTent.cs
@@ -1,26 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetUpTent : MonoBehaviour
 {
+    public GameObject thing;
 
-
+    void Start () 
+    {
+    }
+	
+    void Update () 
+    {
+		
+    }
     void OnTriggerEnter(Collider other)
     {
+        thing.SetActive(true);
+        Debug.Log("TentCollision");
+  }
 
-            Debug.Log("TentCollision");
-  
-    }
 
 
-	void Start () 
-	{
-		
-	}
-	
-	void Update () 
-	{
-		
-	}
 }
