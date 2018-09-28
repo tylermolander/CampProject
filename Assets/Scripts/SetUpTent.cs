432dc07716c6b59755e020ae6d7c9fa01074d4ca
Tyler Molander <tylermolander@gmail.com>
2018-09-27 23:21:32 -0400

    TentCollider

---
+++ Assets/Scripts/SetUpTent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetUpTent : MonoBehaviour
+{
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+            Debug.Log("TentCollision");
+  
+    }
+
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
