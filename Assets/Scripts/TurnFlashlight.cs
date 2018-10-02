858c9a7a1896b828b8771966a7e2cd98ef5c9e1f
Tyler Molander <tylermolander@gmail.com>
2018-10-02 13:01:37 -0400

    FlashlightTurn&Bool

---
+++ Assets/Scripts/TurnFlashlight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnFlashlight : MonoBehaviour
+{
+
+    public Light flashlight;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    flashlight = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	    {
+	        if(Input.GetKeyUp(KeyCode.F))
+	        {
+	            flashlight.enabled = !flashlight.enabled;
+	        }
+	    }
+	}
+}
