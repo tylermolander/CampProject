e7fc4c89347e166cc06a3efaa2c4286a36c4ac53
Tyler Molander <tylermolander@gmail.com>
2018-10-07 19:30:47 -0400

    Replay

---
+++ Assets/Scripts/AfterEndingActivation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class AfterEndingActivation : MonoBehaviour
+{
+    void OnEnable()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("EndingScene", LoadSceneMode.Single);
+    }
+}
