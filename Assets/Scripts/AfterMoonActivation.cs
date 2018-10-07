5d99e48cd95064dac39a0c9c4fe31ca37fc2bc44
Tyler Molander <tylermolander@gmail.com>
2018-10-07 13:22:02 -0400

    TentTransition

--- Assets/Scripts/AfterMoonActivation.cs
+++ Assets/Scripts/AfterMoonActivation.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
  
-public class LoadOnActivation : MonoBehaviour
+public class AfterMoonActivation : MonoBehaviour
 {
     void OnEnable()
     {
