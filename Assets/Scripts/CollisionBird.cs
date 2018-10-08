030e27bda26f247ddc51775d2d567a52115886e4
Tyler Molander <tylermolander@gmail.com>
2018-10-08 00:33:50 -0400

    Bird

---
+++ Assets/Scripts/CollisionBird.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Playables;
+
+public class CollisionBird : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    public PlayableDirector stopTimeline;
+    public GameObject bird;
+    public bool istriggered = false; 
+
+    void Start () 
+    {        
+        timeline = GetComponent<PlayableDirector>();        
+        stopTimeline = bird.GetComponent<PlayableDirector>();
+
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+      Debug.Log("Collision");
+
+        if  (istriggered == false)
+        {
+        timeline.Play();
+        stopTimeline.Stop();
+        }
+    } 
+    void OnTriggerExit( Collider other)
+    {
+        istriggered  = true;
+    }
+}
