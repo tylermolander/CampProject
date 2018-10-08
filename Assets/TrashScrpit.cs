cd6bb95b81048f91515ea61f02ff4fbc97843cc7
Tyler Molander <tylermolander@gmail.com>
2018-10-08 19:14:57 -0400

    BeforeTrashAnimationAttempt

---
+++ Assets/TrashScrpit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashScrpit : MonoBehaviour {
+
+    public bool hasTrash;
+    public GameObject trash;
+
+	void Start () 
+    {
+		
+	}
+	
+    //maybe add a "remaining trash" counter
+	void Update () 
+	{
+		//picking up trash > trashobject destroyed, hasTrash is true
+        //if false:
+            //loose the game: "You forgot to take out your trash!!!"
+
+
+	}
+}
