d23b1db808ad2d24b4d3409cdb2112263314c053
Tyler Molander <tylermolander@gmail.com>
2018-10-07 21:47:57 -0400

    Refactor3

--- Assets/Scripts/CollisionTrigger.cs
+++ Assets/Scripts/CollisionTrigger.cs
@@ -10,7 +10,6 @@ public class CollisionTrigger : MonoBehaviour
     
     void Start () 
     { 
-        Cursor.visible = false;
     }
     
     void OnTriggerEnter(Collider other)
--- Assets/Scripts/TakeDownTentScript.cs
+++ Assets/Scripts/TakeDownTentScript.cs
@@ -12,7 +12,6 @@ public class TakeDownTentScript : MonoBehaviour
 
     void Start () 
     { 
-        Cursor.visible = false;
         timeline = GetComponent<PlayableDirector>();
     }
 	
