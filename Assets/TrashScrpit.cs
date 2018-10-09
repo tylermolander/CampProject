be582c4a9e7d6f7d90964124ddc93e68e229114c
Tyler Molander <tylermolander@gmail.com>
2018-10-08 21:04:58 -0400

    PickupVirtCamFix

--- Assets/TrashScrpit.cs
+++ Assets/TrashScrpit.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class TrashScrpit : MonoBehaviour {
 
-    public bool hasTrash;
-    public GameObject trash;
+    //public bool hasTrash;
+    //public GameObject trash;
+    Animator anim;
+    public GameObject charachter;
 
-	void Start () 
-    {
-		
+	void Start ()
+	{
+	    anim = charachter.GetComponent<Animator>();
 	}
 	
     //maybe add a "remaining trash" counter
+    //loose the game: "You forgot to take out your trash!!!"
+
 	void Update () 
 	{
-		//picking up trash > trashobject destroyed, hasTrash is true
-        //if false:
-            //loose the game: "You forgot to take out your trash!!!"
-
-
 	}
+
+    void OnMouseDown()
+    {
+        anim.SetTrigger("pickUp");
+        //destrory trash
+
+        //walk to trash animation
+    }
 }
